72b1b7e25ccefaa8236f17c4591d8e537c0f3e30
beyhangl@gmail.com <beyhangl@gmail.com>
2017-03-18 13:04:24 +0300

    week1 changes

---
+++ src/main/scala/example/Main.scala
@@ -0,0 +1,11 @@
+package example
+/**
+  * Created by kafein on 18.03.2017.
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+     println("Test")
+    val l = List(1, 32, 5, 11, -1, -3, -5)
+    println(Lists.max(l))
+  }
+}
